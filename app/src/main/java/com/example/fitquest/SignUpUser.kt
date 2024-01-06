2168ba4bf4e8e5640f101b6fe8a46cba0f507233
mariacjeronimo <mariacjeronimo02@gmail.com>
2024-01-06 16:51:00 +0000

    update page complete profile

--- app/src/main/java/com/example/fitquest/SignUpUser.kt
+++ app/src/main/java/com/example/fitquest/SignUpUser.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitquest.databinding.ActivityWelcomeBinding
 import com.example.fitquest.databinding.UserSignUpBinding
@@ -24,6 +26,12 @@ class SignUpUser : AppCompatActivity() {
         binding = UserSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val errorMessageTextView = binding.errorMessageTextView
+        val spinner: Spinner = findViewById(R.id.spinnerGender)
+        val genderOptions = arrayOf("Male", "Female","Other")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
         TextName = findViewById(R.id.editTextName)
         TextUsername = findViewById(R.id.editTextUsername)
@@ -32,12 +40,21 @@ class SignUpUser : AppCompatActivity() {
         buttonSubmit.setOnClickListener {
             val name = TextName.text.toString()
             val username = TextUsername.text.toString()
+            val selectedGender = spinner.selectedItem.toString()
 
             authManager.signUpUser(name, username) { success, errorMessage ->
                 if (success) {
                     Log.d("SignUpUser", "User criado com sucesso")
 
                     val intent = Intent(this@SignUpUser, MainActivity::class.java)
+
+                    // Para direcionar para um questionario diferente dependendo do genero
+
+                   // val intent = when (selectedGender) {
+                   //     "Male" -> Intent(this@SignUpUser, MaleActivity::class.java)
+                   //     "Female" -> Intent(this@SignUpUser, FemaleActivity::class.java)
+                   //     else -> Intent(this@SignUpUser, OtherActivity::class.java)
+                   // }
                     startActivity(intent)
                     finish()
                 } else {
